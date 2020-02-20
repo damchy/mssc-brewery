fc030d3960497212a8c15fa64099e84adbe25efd
damchy <damchy@gmail.com>
2020-02-20 11:10:49 +0100

    Beer put method

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 
 public interface BeerService {
     BeerDto getBeerById(UUID beerId);
+    BeerDto saveNewBeer(BeerDto beerDto);
+    void updateBeer(UUID beerId, BeerDto beerDto);
 }
 
 
